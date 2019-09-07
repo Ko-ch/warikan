cd9973e2bfab483fbbf035a08dd524bd0ad5da56
Koichiro <koichiro_takagi@outlook.jp>
2019-09-08 00:25:15 +0900

    他のファイルの関数をimportして使用するようにしたよ。 from ファイル名 import 関数名

---
+++ app4.py
@@ -0,0 +1,4 @@
+# 割り勘計算したい。6人で７０００円。
+from warikan import cal_payment
+
+cal_payment(7000, 6)
--- warikan.py
+++ warikan.py
@@ -1,5 +1,4 @@
-def warikan(amount, number_of_people):
+def cal_payment(amount, number_of_people):
     print(f"一人当たり：{amount // number_of_people}円、 端数：{amount % number_of_people}円")
 
-
-warikan(int(input('合計金額は？')), int(input("合計人数は？")))
+# cal_payment(int(input('合計金額は？')), int(input("合計人数は？")))
